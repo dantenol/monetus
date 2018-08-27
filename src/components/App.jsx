a4877af72378ae48d35beb652c8a691b02156c21
Dante Nolasco <dnolsacodante@gmail.com>
2018-08-27 12:48:33 -0300

    initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,61 @@
+import React, { Component } from "react";
+import axios from "axios";
+import "../assets/styles/index.css";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      symbol: "",
+      data: {}
+    };
+    this.handleCheckValue = this.handleCheckValue.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handlePressEnter = this.handlePressEnter.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ symbol: event.target.value });
+  }
+
+  handlePressEnter(event) {
+    if (event.keyCode === 13) this.handleCheckValue();
+  }
+
+  async handleCheckValue() {
+    const { symbol } = this.state;
+    const res = await axios(
+      `https://api.iextrading.com/1.0/stock/${symbol}/quote`
+    );
+    this.setState({ data: res.data });
+  }
+
+  render() {
+    console.log(this.state);
+    return (
+      <div className="container">
+        <div className="search">
+          <input
+            onKeyDown={this.handlePressEnter}
+            value={this.state.symbol}
+            onChange={this.handleChange}
+          />
+          <button onClick={this.handleCheckValue}>Ver valor da ação</button>
+        </div>
+        {this.state.data.latestPrice && (
+          <div className="information">
+            <div className="name">
+              <p>{this.state.data.companyName}&nbsp;</p>
+              <span className="symbol">{this.state.data.symbol}</span>
+            </div>
+            <div className="price">
+              <p>${this.state.data.latestPrice}</p>
+            </div>
+          </div>
+        )}
+      </div>
+    );
+  }
+}
+
+export default App;
